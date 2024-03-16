9bbae811cb22812c2087cb859a5da06f2b5f6d12
Esmael <esmael5270806@gmail,com>
2024-03-16 08:28:48 +0200

    Initial commit

---
+++ app/src/main/java/com/example/loginsignup/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.loginsignup;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        gotoLoginFragment();
+    }
+
+    private void gotoLoginFragment() {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.frameLyoutMain, new LoginFragment());
+        ft.commit();
+    }
+}
